28631cc1d8717e962b9fb26d2714c0d61212406b
Davi Castello Branco Tavares de Oliveira <davicbtoliveira@gmail.com>
2026-04-25 17:13:19 -0400

    FEAT: Modular config and `command_map.go` added

---
+++ command_map.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func commandMap(c *config) error {
+	locationsResp, err := c.pokeapiClient.ListLocations(c.Next)
+	if err != nil {
+		return err
+	}
+
+	c.Next = locationsResp.Next
+	c.Previous = locationsResp.Previous
+
+	for _, v := range locationsResp.Results {
+		fmt.Println(v.Name)
+	}
+
+	return nil
+}
+
+func commandMapb(c *config) error {
+	if c.Previous == nil {
+		return errors.New("you're on the first page")
+	}
+
+	locationsResp, err := c.pokeapiClient.ListLocations(c.Previous)
+	if err != nil {
+		return nil
+	}
+
+	c.Next = locationsResp.Next
+	c.Previous = locationsResp.Previous
+
+	for _, v := range locationsResp.Results {
+		fmt.Println(v.Name)
+	}
+
+	return nil
+}
