c1a30a2cb12c02d672fa04f20f764069e151eabe
Davi Castello Branco Tavares de Oliveira <davicbtoliveira@gmail.com>
2026-04-27 19:37:08 -0400

    FEAT: `commandCatch` to catch pokemons

---
+++ command_catch.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func commandCatch(cfg *config, param *string) error {
+	if param == nil {
+		return fmt.Errorf("you must provide a pokemon name")
+	}
+
+	pokeInfo, err := cfg.pokeapiClient.PokemonInfo(param)
+	if err != nil {
+		return err
+	}
+
+	baseXP := pokeInfo.BaseExperience
+
+	fmt.Printf("Throwing a Pokeball at %v...\n", pokeInfo.Name)
+
+	if rand.Intn(350) > baseXP {
+		caught := Pokemon{
+			Name:           pokeInfo.Name,
+			BaseExperience: pokeInfo.BaseExperience,
+			Height:         pokeInfo.Height,
+			IsDefault:      pokeInfo.IsDefault,
+			Order:          pokeInfo.Order,
+			Weight:         pokeInfo.Weight,
+		}
+		cfg.pokedex["caughts"] = caught
+		fmt.Printf("%v was caught!\n", caught.Name)
+		return nil
+	}
+
+	return fmt.Errorf("%v escaped!", pokeInfo.Name)
+}
